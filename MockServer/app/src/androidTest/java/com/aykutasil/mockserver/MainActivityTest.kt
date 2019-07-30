11b2d6f0ec5d99d426c38740c41d4775770eade4
Aykut Asil <aykuttasil@gmail.com>
2019-07-30 23:04:49 +0300

    Update MainActivityTest.kt

--- MockServer/app/src/androidTest/java/com/aykutasil/mockserver/MainActivityTest.kt
+++ MockServer/app/src/androidTest/java/com/aykutasil/mockserver/MainActivityTest.kt
@@ -3,24 +3,21 @@ package com.aykutasil.mockserver
 import android.content.Context
 import android.content.Intent
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.IdlingResource
-import androidx.test.platform.app.InstrumentationRegistry
-import okhttp3.mockwebserver.MockResponse
-import java.io.File
-
 import java.io.BufferedReader
 
 @RunWith(AndroidJUnit4::class)
@@ -29,20 +26,21 @@ class MainActivityTest {
     private lateinit var mIdlingResource: IdlingResource
     private lateinit var mockWebServer: MockWebServer
 
-    private val activityRule = object : ActivityTestRule<MainActivity>(MainActivity::class.java, false, false) {
-        override fun afterActivityLaunched() {
-            super.afterActivityLaunched()
+    private val activityRule =
+        object : ActivityTestRule<MainActivity>(MainActivity::class.java, false, false) {
+            override fun afterActivityLaunched() {
+                super.afterActivityLaunched()
 
-            // Activity çalışmaya başladıktan sonra içerisindeki nesnelere erişim sağlayabiliriz.
-            // Activity içerisinde oluşturulan CountingIdlingResource nesnesine erişerek, asenkron süreçlerin tamamlanıp tamamlanmadığını kontrol edebiliriz.
-            mIdlingResource = activity.idlingResource
+                // Activity çalışmaya başladıktan sonra içerisindeki nesnelere erişim sağlayabiliriz.
+                // Activity içerisinde oluşturulan CountingIdlingResource nesnesine erişerek, asenkron süreçlerin tamamlanıp tamamlanmadığını kontrol edebiliriz.
+                mIdlingResource = activity.idlingResource
 
-            // Espresso'ya IdlingResource nesnesi vererek eğer asenkron bir süreç var ise beklemesini ve bittikten sonra devam etmesini söylüyoruz.
-            // Espresso'nun Hangi durumda bekleyip hangi durumda devam edeceğini MainActivity içerisinde
-            // CountingIdlingResource nesnesinin increment() veya decrememt() fonksiyonlarını çağırarak belirtiyoruz.
-            IdlingRegistry.getInstance().register(mIdlingResource)
+                // Espresso'ya IdlingResource nesnesi vererek eğer asenkron bir süreç var ise beklemesini ve bittikten sonra devam etmesini söylüyoruz.
+                // Espresso'nun Hangi durumda bekleyip hangi durumda devam edeceğini MainActivity içerisinde
+                // CountingIdlingResource nesnesinin increment() veya decrememt() fonksiyonlarını çağırarak belirtiyoruz.
+                IdlingRegistry.getInstance().register(mIdlingResource)
+            }
         }
-    }
 
     @Before
     fun setUp() {
@@ -63,7 +61,6 @@ class MainActivityTest {
     @After
     fun tearDown() {
         // Her test sonrası MockWebServer kapatıyoruz ce IdlingResource nesnemizi unregister ediyoruz.
-
         mockWebServer.close()
         IdlingRegistry.getInstance().unregister(mIdlingResource)
     }
@@ -87,7 +84,6 @@ class MainActivityTest {
 }
 
 object RestServiceTestHelper {
-
 
     @Throws(Exception::class)
     fun getStringFromFile(context: Context, filePath: String): String {
